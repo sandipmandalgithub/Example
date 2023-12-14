33c5630f55878665dfbaa28426fae991183b4f82
sandipmandalgithub <sandipmandal4055@gmail.com>
2023-12-14 13:46:11 +0530

    create a user class

---
+++ src/com/jsp/example/User.java
@@ -0,0 +1,17 @@
+package com.jsp.example;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String userEmail;
+	private String userPassword;
+	
+	public int getUserId() {
+	return userId;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail=userEmail;
+	}
+	
+
+}
